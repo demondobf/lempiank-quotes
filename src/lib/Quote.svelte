52bfb593e9e9b22fd57449513588657c0ced598a
demondo <demondocs1.6@gmail.com>
2021-05-31 12:35:58 +0200

    initial

---
+++ src/lib/Quote.svelte
@@ -0,0 +1,12 @@
+<h1>This is quote</h1>
+<time datetime="14/12/2020">14/12/2020</time>
+
+<style>
+	h1 {
+		font-size: clamp(2rem, 4vw, 4rem);
+	}
+
+	time {
+		font-size: 1.5rem;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Quote from '$lib/Quote.svelte';
+</script>
+
+<Quote />
