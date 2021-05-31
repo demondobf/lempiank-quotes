9f3c57afb501f3e1926bed4b9e984da42f554a17
demondo <demondocs1.6@gmail.com>
2021-05-31 18:27:47 +0200

    added Preloader component

---
+++ src/lib/components/Preloader.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	const messages = [
+		'Szukam głupot...',
+		'Ładuję cytaty...',
+		'Trzeba poczekać troszkę...',
+		'Sprawdzam co Lempiank mówiła...'
+	];
+	const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+</script>
+
+<template>
+	<h1>{randomMessage}</h1>
+</template>
