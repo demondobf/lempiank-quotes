767e6c0980a9e3917fd24351b8a362550ae7c924
demondo <demondocs1.6@gmail.com>
2021-05-31 19:47:49 +0200

    added vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
 import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess(),
   kit: {
     target: '#svelte',
+    adapter: vercel(),
   },
 };
 
