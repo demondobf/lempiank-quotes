d54ef70a3762a30440caf77f7a815937e73bfc51
demondo <demondocs1.6@gmail.com>
2021-05-31 18:27:15 +0200

    refactored Photo component

--- src/lib/components/Photo.svelte
+++ src/lib/components/Photo.svelte
@@ -3,17 +3,11 @@
 	import { LEMPIANK_INSTAGRAM } from '$lib/constants';
 </script>
 
-<a href={LEMPIANK_INSTAGRAM} rel="noopener noreferrer" target="_blank" transition:fade>
-	<picture>
-		<source srcset="images/lempiank.webp" type="image/webp" />
-		<img src="images/lempiank.png" alt="Lempiank" width="500" height="500" />
-	</picture>
-</a>
-
-<style>
-	img {
-		height: auto;
-		max-width: 100%;
-		filter: grayscale(1);
-	}
-</style>
+<template>
+	<a href={LEMPIANK_INSTAGRAM} rel="noopener noreferrer" target="_blank" transition:fade>
+		<picture>
+			<source srcset="images/lempiank.webp" type="image/webp" />
+			<img src="images/lempiank.png" alt="Lempiank" width="500" height="500" />
+		</picture>
+	</a>
+</template>
